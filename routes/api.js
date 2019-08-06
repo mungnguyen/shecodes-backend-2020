4d9ed2fc1924ba4f54eae49f3f0af08dc746d2b2
Mung Nguyen <mungyp98@gmail.com>
2019-08-06 22:39:42 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,12 @@ module.exports = function(app) {
   const ToDo = require('../controller/todoController')
   const Information = require('../controller/informationController')
 
-  app.get('/api/getAllTodo', ToDo.getAllTodo)
-  app.post('/api/addTodo', ToDo.addTodo)
-  app.put('/api/updateTodo/:id', ToDo.updateTodo)
-  app.delete('/api/deleteTodo/:id', ToDo.deleteTodo)
-
+  // Lấy tất cả info - thông tin đăng kí của ứng viên
   app.get('/api/information', Information.getAllInformation)
+
+  // Lấy thông tin đăng kí theo id
+  app.get('api/information/:id', Information.getAllInformationById)
+
+  // Thêm 1 thông tin đăng kí mới, truyền các trường hôm nọ m gửi t ấy, chưa có vadidate
+  app.post('api/information', Information.addInformation)
 }
